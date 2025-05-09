e2fcb704c162eae70d17a0ce17bdde81178f8a3f
Andros Guirado <andros.guirado@feelslike.studio>
2025-05-09 12:45:50 +0200

    Update main branch with local files

---
+++ src/script.js
@@ -0,0 +1,143 @@
+import { gsap } from 'gsap'
+import { SplitText } from 'gsap/SplitText'
+import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin'
+// -
+gsap.registerPlugin(SplitText, DrawSVGPlugin);
+
+import { masks } from './chars'
+
+// -
+const svgns = "http://www.w3.org/2000/svg";
+
+const TEST = false;
+
+// -
+class WrittenChar {
+	constructor(container, char, value, key, color, speed, delay, index, ease) {
+		this.speed = speed || 200;
+		this.delay = delay || 0;
+
+		this.ease = ease || 'sine.in';
+		// -
+		this.container = container;
+		this.char = char;
+		this.value = value;
+		this.key = key;
+		this.color = color || '#000';
+		this.index = index;
+		// -
+		this.originalSize = { w: this.value.w, h: this.value.h };
+		this.strokeWidth = this.originalSize.h * 0.07;
+		// -
+		this.ratio = this.originalSize.w / this.originalSize.h;
+		this.size = { w: this.container.clientHeight * this.ratio, h: this.container.clientHeight };
+		this.init();
+	}
+
+	init() {
+		this.svg = document.createElementNS(svgns, "svg");
+		this.svg.setAttribute("xmlns", svgns);
+		this.svg.setAttribute("width", this.originalSize.w);
+		this.svg.setAttribute("height", this.originalSize.h);
+		this.svg.setAttribute("viewBox", `0 0 ${this.originalSize.w} ${this.originalSize.h}`);
+		this.container.appendChild(this.svg);
+
+		const defs = document.createElementNS(svgns, "defs");
+		this.svg.appendChild(defs);
+
+		this.mask = document.createElementNS(svgns, "mask");
+		this.mask.setAttribute("id", `mask-${this.key}-${this.index}`);
+		this.mask.setAttribute("x", "0");
+		this.mask.setAttribute("y", "0");
+		this.mask.setAttribute("width", this.originalSize.w);
+		this.mask.setAttribute("height", this.originalSize.h);
+		this.mask.style.maskType = "alpha";
+		defs.appendChild(this.mask);
+		//*/
+
+
+		// - Make a path
+		this.path = document.createElementNS(svgns, "path");
+		this.path.setAttribute("id", `path-${this.key}-${this.index}`);
+		this.path.setAttribute("d", this.value.mask);
+
+		//*
+		this.text = document.createElementNS(svgns, "text");
+		this.text.setAttribute("x", "2%");
+		this.text.setAttribute("y", "76%");
+		this.text.setAttribute("font-family", "FeelsLikeEcomm");
+		this.text.setAttribute("font-size", this.originalSize.h * 0.83);
+		this.text.setAttribute("font-weight", "100");
+		this.text.setAttribute("font-size-adjust", this.ratio);
+		this.text.setAttribute("fill", this.color);
+		this.text.setAttribute("text-anchor", "start");
+		this.text.textContent = this.key;
+
+
+		// this.char.style.maskImage = `url(data:image/svg+xml,${this.svg.outerHTML})`;
+
+		// - TEST
+		if (TEST) {
+			this.svg.appendChild(this.path);
+			this.text.setAttribute("fill", 'magenta');
+
+		} else {
+			this.mask.appendChild(this.path);
+			this.text.setAttribute("mask", `url(#mask-${this.key}-${this.index})`);
+		}
+		//*/
+		this.svg.appendChild(this.text);
+
+		gsap.set(this.path, {
+			fill: 'none',
+			stroke: '#000',
+			strokeWidth: this.strokeWidth,
+			strokeLinecap: 'round',
+			strokeLinejoin: 'round'
+		});
+
+		//const duration = this.path.getTotalLength() / this.speed;
+
+		gsap.from(this.path, {
+			duration: this.speed,
+			delay: this.delay,
+			ease: this.ease,
+			drawSVG: 0
+		});
+
+	}
+}
+
+
+function writeText(element, color = '#000', speed = 0.1, charDelay = 0.0, wordDelay = 0.0, ease = 'sine.in') {
+	// -
+	const splitText = new SplitText(element, {
+		type: "words,chars",
+		mask: "chars",
+		charsClass: "char",
+		wordsClass: "word",
+		smartWrap: true,
+		autoSplit: true,
+		onSplit(self) {
+			let delay = 1.0;
+			let index = 0;
+			for (const word of self.words) {
+				for (const container of word.children) {
+					const char = container.querySelector('.char');
+					const key = char.textContent.toUpperCase();
+					if (key) {
+						const value = masks[key];
+						if (value) {
+							new WrittenChar(container, char, value, key, color, speed, delay, index, ease);
+							index++;
+							delay += charDelay;
+						}
+					}
+				}
+				delay += wordDelay;
+			};
+		}
+	});
+}
+
+writeText(document.querySelector('.written-text'), '#000', .220, .222, 0.2, 'sine.out');
